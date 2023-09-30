7cec91cb1feb25dd28d7d9f5a953a64f17f647bd
Scott Hampton <felgamedev@gmail.com>
2023-09-30 11:22:48 -0300

    Fix import for profile controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\ProfileUpdateRequest;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Http\RedirectResponse;
