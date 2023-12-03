2e503856f99a50435104f456626a65a8ac217fb6
Scott Hampton <felgamedev@gmail.com>
2023-12-03 16:59:19 -0400

    Ensure role code is unique in the account

--- app/Http/Requests/Roles/RoleCreateRequest.php
+++ app/Http/Requests/Roles/RoleCreateRequest.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Requests\Roles;
 
+use App\Models\Role;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class RoleCreateRequest extends FormRequest
 {
@@ -17,7 +19,7 @@ class RoleCreateRequest extends FormRequest
         return [
             'name' => 'required|string|max:255',
             'description' => 'required|string|max:255',
-            'role_code' => 'required|string|max:64',
+            'role_code' => ['required', 'string', 'max:64', Rule::unique(Role::class)->where('account_id', $this->user()->account->id)],
         ];
     }
 }
