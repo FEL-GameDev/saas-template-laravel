4641f9cbe43058c700bfcc9430062b3501e3ac98
Scott Hampton <felgamedev@gmail.com>
2024-02-13 18:53:07 -0400

    Linting SendUserInvitedNotifications.php

--- app/Listeners/SendUserInvitedNotifications.php
+++ app/Listeners/SendUserInvitedNotifications.php
@@ -2,12 +2,10 @@
 
 namespace App\Listeners;
 
-use App\Models\UserInvite;
 use App\Events\UserInviteCreated;
 use App\Notifications\UserInvitedNotification;
 use App\Services\UserInvite\GetUserInvite;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 
 class SendUserInvitedNotifications implements ShouldQueue
 {
@@ -24,7 +22,6 @@ class SendUserInvitedNotifications implements ShouldQueue
      */
     public function handle(UserInviteCreated $event): void
     {
-
         $invitedUserToNotify = GetUserInvite::getById($event->userInvite->id);
         $invitedUserToNotify->notify(new UserInvitedNotification($event->userInvite));
     }
