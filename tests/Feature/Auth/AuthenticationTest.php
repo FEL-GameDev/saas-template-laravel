0c36a3c872f90d53a97e27828b416c7b60b37a35
Scott Hampton <felgamedev@gmail.com>
2024-03-01 19:04:22 -0400

    Fix for Authentication test

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -20,7 +20,7 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_authenticate_using_the_login_screen(): void
     {
-        $user = User::factory()->create();
+        $user = User::factory()->create(['password' => 'password']);
 
         $response = $this->post('/login', [
             'email' => $user->email,
