755eb7e81f031ea0bc0e660cffb8e8ecd9d18f92
Scott Hampton <felgamedev@gmail.com>
2024-03-03 11:50:56 -0400

    Add unique Role role_name constraint for an account

--- tests/Feature/Services/Role/CreateRoleTest.php
+++ tests/Feature/Services/Role/CreateRoleTest.php
@@ -5,6 +5,7 @@ namespace Services\Role;
 use App\DTO\Role\CreateRoleDTO;
 use App\Models\Account;
 use App\Services\Role\CreateRole;
+use Exception;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -20,5 +21,26 @@ class CreateRoleTest extends TestCase
         $role = CreateRole::create($roleDTO);
 
         $this->assertNotNull($role);
+    }
+
+    public function testCreateRole_failsOnDuplicateRoleCode(): void
+    {
+        $account = Account::factory()->create();
+        $roleDTO = CreateRoleDTO::create(
+            accountId: $account->id,
+            roleCode: 'super_admin',
+            name: fake()->text(),
+            description: fake()->name()
+        );
+        CreateRole::create($roleDTO);
+        $roleDTO2 = CreateRoleDTO::create(
+            accountId: $account->id,
+            roleCode: 'super_admin',
+            name: fake()->text(),
+            description: fake()->name()
+        );
+
+        $this->expectException(Exception::class);
+        CreateRole::create($roleDTO2);
     }
 }
