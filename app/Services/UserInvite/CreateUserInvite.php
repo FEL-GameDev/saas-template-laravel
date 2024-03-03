bc5bffa9b2e2b9e15d3323b07b44a3b6c45cb24a
Scott Hampton <felgamedev@gmail.com>
2024-03-03 11:02:48 -0400

    Use web safe uuid for invite codes

--- app/Services/UserInvite/CreateUserInvite.php
+++ app/Services/UserInvite/CreateUserInvite.php
@@ -5,14 +5,17 @@ namespace App\Services\UserInvite;
 use App\DTO\CreateUserInviteDTO;
 use App\Models\UserInvite;
 use App\Repositories\UserInvites\CreateUserInviteRepository;
+use Ramsey\Uuid\Uuid;
 
 class CreateUserInvite
 {
     public static function create(CreateUserInviteDTO $createUserInviteDTO): UserInvite
     {
         $createUserInviteRepository = new CreateUserInviteRepository();
-
-        $inviteCode = hash("crc32", $createUserInviteDTO->email . $createUserInviteDTO->accountId);
+        
+        $uuid4 = Uuid::uuid4();
+        $inviteCode = strtr(base64_encode($uuid4->getBytes()), '+/', '-_');
+        $inviteCode = rtrim($inviteCode, '=');
 
         return $createUserInviteRepository->create($createUserInviteDTO, inviteCode: $inviteCode);
     }
