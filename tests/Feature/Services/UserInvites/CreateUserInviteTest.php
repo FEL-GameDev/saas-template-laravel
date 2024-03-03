54f6a8e4edb5222f0a6ba147023c3d391ad0a48b
Scott Hampton <felgamedev@gmail.com>
2024-03-03 11:51:50 -0400

    Add unique UserInvite email constraint for an account

--- tests/Feature/Services/UserInvites/CreateUserInviteTest.php
+++ tests/Feature/Services/UserInvites/CreateUserInviteTest.php
@@ -5,13 +5,14 @@ namespace Services\UserInvites;
 use App\DTO\CreateUserInviteDTO;
 use App\Models\User;
 use App\Services\UserInvite\CreateUserInvite;
+use Exception;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class CreateUserInviteTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     public function testCreateUserInvite()
     {
         $user = User::factory()->create();
@@ -26,4 +27,25 @@ class CreateUserInviteTest extends TestCase
 
         $this->assertNotNull($userInvite);
     }
+
+    public function testCreateUserInvite_failsOnNonUniqueEmail()
+    {
+        $user = User::factory()->create();
+        $createUserInviteDTO1 = CreateUserInviteDTO::create(
+            name: 'John Doe',
+            email: 'test@example.com', userId: $user->id,
+            accountId: $user->account_id
+        );
+        CreateUserInvite::create($createUserInviteDTO1);
+        $createUserInviteDTO2 = CreateUserInviteDTO::create(
+            name: 'John Ray',
+            email: 'test@example.com',
+            userId: $user->id,
+            accountId: $user->account_id
+        );
+
+        $this->expectException(Exception::class);
+        CreateUserInvite::create($createUserInviteDTO2);
+    }
+
 }
