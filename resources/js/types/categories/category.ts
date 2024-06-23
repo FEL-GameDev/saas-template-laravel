348df569402a7df57a48e106d2b703127fef7719
Scott Hampton <felgamedev@gmail.com>
2024-06-23 12:26:58 -0300

    Update Category types

--- resources/js/types/categories/category.ts
+++ resources/js/types/categories/category.ts
@@ -3,6 +3,21 @@ export interface Category {
     name: string;
     description?: string;
     sub_categories_count?: number;
+    sub_categories?: SubCategory[];
     created_at: string;
     updated_at: string;
+}
+
+export interface SubCategory {
+    id: number;
+    name: string;
+    description?: string;
+    category_id: number;
+    created_at: string;
+    updated_at: string;
+}
+
+export interface SubCategoryCreate {
+    name: string;
+    description?: string;
 }
